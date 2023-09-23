461d94bc5a7854fd3cbf5065ff2f9f5a518de314
banshay <importantbopp@gmail.com>
2023-09-23 22:41:04 +0200

    ignore resolve

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
     base: "/new-world-buddy-item-management/",
     plugins: [vue()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
 })
