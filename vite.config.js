c2121a95e42023e7053e51403bbdddb790732485
banshay <importantbopp@gmail.com>
2023-09-24 14:44:09 +0200

    hardcode base path

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 const path = require('path')
 
-const basepath = process.env.PROD ? "/new-world-buddy-item-management/" : "/";
+// const basepath = process.env.PROD ? "/new-world-buddy-item-management/" : "/";
 
 export default defineConfig({
-  path: basepath,
+  path: "/new-world-buddy-item-management/",
     plugins: [vue()],
     resolve: {
         alias: {
