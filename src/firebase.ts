7421b1fb3e10cbaee477cc3d151fc7f8a827170b
banshay <importantbopp@gmail.com>
2023-09-24 13:33:54 +0200

    Fix vite switch and firestore access

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,28 +1,26 @@
 // Import the functions you need from the SDKs you need
 // import '@firebase/app'
 // import '@firebase/firestore'
-import { onUnmounted, ref } from 'vue'
-import firebase from 'firebase/compat'
+import {onUnmounted, ref} from 'vue'
+import {initializeApp} from "firebase/app"
+import {getFirestore, collection, addDoc, deleteDoc, doc, updateDoc, onSnapshot} from "firebase/firestore"
 import {
-  getAuth,
-  getRedirectResult,
-  signInWithRedirect,
-  signInWithPopup,
-  User,
-} from 'firebase/auth'
-import initializeApp = firebase.initializeApp
-import GoogleAuthProvider = firebase.auth.GoogleAuthProvider
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+    getAuth,
+    getRedirectResult,
+    signInWithRedirect,
+    GoogleAuthProvider,
+    signInWithPopup,
+    type User,
+} from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCzMVjUW48VRTYSZdD4Q0aZ8oCy57oOR80',
-  authDomain: 'new-world-buddy.firebaseapp.com',
-  projectId: 'new-world-buddy',
-  storageBucket: 'new-world-buddy.appspot.com',
-  messagingSenderId: '743241756745',
-  appId: '1:743241756745:web:437237b8b22ce104b70f98',
+    apiKey: 'AIzaSyCzMVjUW48VRTYSZdD4Q0aZ8oCy57oOR80',
+    authDomain: 'new-world-buddy.firebaseapp.com',
+    projectId: 'new-world-buddy',
+    storageBucket: 'new-world-buddy.appspot.com',
+    messagingSenderId: '743241756745',
+    appId: '1:743241756745:web:437237b8b22ce104b70f98',
 }
 
 // Initialize Firebase
@@ -34,43 +32,42 @@ const currentUser = ref<User | null>(null)
 
 // export const signIn = () => signInWithRedirect(auth, provider)
 export const signIn = async () => {
-  const result = await signInWithPopup(auth, provider)
-  currentUser.value = result?.user
+    const result = await signInWithPopup(auth, provider)
+    currentUser.value = result?.user
 }
 export const isLoggedIn = async () => {
     return !!auth.currentUser
 }
 
-const db = app.firestore()
-export const itemCollection = db.collection('items')
-export const categoryCollection = db.collection('categories')
+const db = getFirestore(app)
+export const itemCollection = collection(db, 'items')
+export const categoryCollection = collection(db, 'categories')
 
 export interface Item {
-  id?: string
-  name: string
-  category: string
-  tier: number | null
-  ingredients: Ingredient[]
+    id?: string
+    name: string
+    category: string
+    tier: number | null
+    ingredients: Ingredient[]
 }
 
 export interface Ingredient {
-  name: string
-  quantity: number | null
+    name: string
+    quantity: number | null
 }
 
-export const addItem = (item: Item) => itemCollection.add(item)
-export const deleteItem = (id: string) => itemCollection.doc(id).delete()
-export const updateItem = (id: string, item: Item) =>
-  itemCollection.doc(id).update(item)
+export const addItem = (item: Item) => addDoc( itemCollection, item)
+export const deleteItem = (id: string) => deleteDoc(doc(itemCollection, id))
+export const updateItem = (id: string, item: Item) => updateDoc(doc(itemCollection, id), {...item})
 
 export const useLoadItems = () => {
-  const items = ref<Item[]>([])
-  const close = itemCollection.onSnapshot((snapshot: any) => {
-    items.value = snapshot.docs.map((item: any) => ({
-      id: item.id,
-      ...item.data(),
-    }))
-  })
-  onUnmounted(close)
-  return items
+    const items = ref<Item[]>([])
+    const close = onSnapshot(itemCollection, (snapshot: any) => {
+        items.value = snapshot.docs.map((item: any) => ({
+            id: item.id,
+            ...item.data(),
+        }))
+    })
+    onUnmounted(close)
+    return items
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const path = require('path')
+// base: import.meta.env.PROD ? "/new-world-buddy-item-management/" : "/",
+
 export default defineConfig({
-    base: "/new-world-buddy-item-management/",
     plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src")
+        }
+    }
 })
